c4cc8d64ad6d07cfe7da6ef6eb990b3702dcff8a
Srinath Senthilkumar <145647472+srinath7796@users.noreply.github.com>
2025-11-08 20:14:58 +0000

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // we disabled bodyParser inside the route, nothing else needed here
-};
-
-module.exports = nextConfig;
