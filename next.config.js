b4a1aab36551e43b78e47fd4f9a3fa4063e6aabe
Srinath Senthilkumar <145647472+srinath7796@users.noreply.github.com>
2025-11-08 20:11:34 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // we disabled bodyParser inside the route, nothing else needed here
+};
+
+module.exports = nextConfig;
