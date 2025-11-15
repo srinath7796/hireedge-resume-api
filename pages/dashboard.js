fa4c89fe87287af3aff2fe308f44f9269eef7228
Srinath Senthilkumar <145647472+srinath7796@users.noreply.github.com>
2025-11-15 15:35:40 +0000

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        color: "#fff",
+        padding: "40px",
+      }}
+    >
+      <h1>Welcome to HireEdge Dashboard</h1>
+      <p>You are logged in successfully via Google.</p>
+    </div>
+  );
+}
